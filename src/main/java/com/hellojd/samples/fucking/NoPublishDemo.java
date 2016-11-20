1eae7eb571dc9d7f8442847fd18805c28349fbec
jeecms <randy_shandong@163.com>
2016-11-20 19:44:37 +0800

    线程不安全范例

---
+++ src/main/java/com/hellojd/samples/fucking/NoPublishDemo.java
@@ -0,0 +1,38 @@
+package com.hellojd.samples.fucking;
+
+public class NoPublishDemo {
+    boolean stop = false;
+
+    public static void main(String[] args) {
+        // LoadMaker.makeLoad();
+
+        NoPublishDemo demo = new NoPublishDemo();
+
+        Thread thread = new Thread(demo.getConcurrencyCheckTask());
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("Set stop to true in main!");
+        demo.stop = true;
+        System.out.println("Exit main.");
+    }
+
+    ConcurrencyCheckTask getConcurrencyCheckTask() {
+        return new ConcurrencyCheckTask();
+    }
+
+    private class ConcurrencyCheckTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("ConcurrencyCheckTask started!");
+            // 如果主线中stop的值可见，则循环会退出。
+            // 在我的开发机上，几乎必现循环不退出！（简单安全的解法：在running属性上加上volatile）
+            while (!stop) {
+            }
+            System.out.println("ConcurrencyCheckTask stopped!");
+        }
+    }
+}
---
+++ src/main/java/com/hellojd/samples/fucking/README.md
@@ -0,0 +1,2 @@
+## 线程不安全的经典范例
++ 来自https://github.com/oldratlee/fucking-java-concurrency
